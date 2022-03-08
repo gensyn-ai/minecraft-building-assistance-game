a659412305f434c40b88fed499d05b23672d4bfa
Cassidy Laidlaw <cassidy256@gmail.com>
2022-03-08 13:03:06 -0800

    Add policy_ids option to rollout.py

--- mbag/rllib/rollout.py
+++ mbag/rllib/rollout.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import List, Optional
 import gym
 import torch
 from datetime import datetime
@@ -26,6 +26,7 @@ def sacred_config():
     checkpoint = ""  # noqa: F841
     episodes = 100  # noqa: F841
     experiment_name = ""  # noqa: F841
+    policy_ids: Optional[List[str]] = None  # noqa: F841
 
     num_workers = 4
     output_max_file_size = 64 * 1024 * 1024
@@ -35,7 +36,8 @@ def sacred_config():
         "evaluation_num_episodes": 1,
         "output_max_file_size": output_max_file_size,
         "evaluation_config": {},
-        "env_config": {},
+        "env_config": {"malmo": {}},
+        "multiagent": {},
         "num_gpus": 1 if torch.cuda.is_available() else 0,
     }
 
@@ -49,6 +51,7 @@ def main(
     checkpoint: str,
     experiment_name: str,
     episodes: int,
+    policy_ids: Optional[List[str]],
     record_video: bool,
     _log,
 ):
@@ -63,20 +66,26 @@ def main(
     out_dir = os.path.join(
         os.path.dirname(checkpoint), f"rollouts_{experiment_name}{time_str}"
     )
-    config_updates.update(
-        {
-            "output": out_dir,
-        }
-    )
+    config_updates["output"] = out_dir
+
+    if policy_ids is not None:
+
+        def policy_mapping_fn(
+            agent_id: str, episode, worker, policy_ids=policy_ids, **kwargs
+        ):
+            agent_index = int(agent_id[len("player_") :])
+            return policy_ids[agent_index]
+
+        config_updates["multiagent"]["policy_mapping_fn"] = policy_mapping_fn
+        config_updates["env_config"]["num_players"] = len(policy_ids)
+        config_updates["env_config"]["malmo"]["player_names"] = policy_ids
 
     if record_video:
-        config_updates["env_config"].update(
+        config_updates["env_config"]["malmo"].update(
             {
-                "malmo": {
-                    "use_malmo": True,
-                    "use_spectator": True,
-                    "video_dir": out_dir,
-                }
+                "use_malmo": True,
+                "use_spectator": True,
+                "video_dir": out_dir,
             }
         )
         config_updates["evaluation_num_workers"] = 1
