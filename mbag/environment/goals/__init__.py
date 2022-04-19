bcae93437c831d2ba18ee86a31926eb9dbf41a3e
Cassidy Laidlaw <cassidy256@gmail.com>
2022-04-19 16:23:16 -0400

    Add cropped GrabCraft goal generator to ALL_GOAL_GENERATORS

--- mbag/environment/goals/__init__.py
+++ mbag/environment/goals/__init__.py
@@ -1,7 +1,7 @@
 from typing import Dict, Type
 
 from .goal_generator import GoalGenerator
-from .grabcraft import GrabcraftGoalGenerator
+from .grabcraft import CroppedGrabcraftGoalGenerator, GrabcraftGoalGenerator
 from .simple import BasicGoalGenerator, RandomGoalGenerator
 from .craftassist import CraftAssistGoalGenerator
 
@@ -9,13 +9,15 @@ ALL_GOAL_GENERATORS: Dict[str, Type[GoalGenerator]] = {
     "basic": BasicGoalGenerator,
     "random": RandomGoalGenerator,
     "grabcraft": GrabcraftGoalGenerator,
+    "cropped_grabcraft": CroppedGrabcraftGoalGenerator,
     "craftassist": CraftAssistGoalGenerator,
 }
 
 __all__ = [
     "ALL_GOAL_GENERATORS",
-    "GrabcraftGoalGenerator",
     "BasicGoalGenerator",
     "RandomGoalGenerator",
+    "GrabcraftGoalGenerator",
+    "CroppedGrabcraftGoalGenerator",
     "CraftAssistGoalGenerator",
 ]
