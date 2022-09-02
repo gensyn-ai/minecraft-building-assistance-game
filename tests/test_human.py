d1b797eb0a3f0e8dbb7f5eea6a5f624a644de617
Timothy G <timg51237@gmail.com>
2022-09-01 22:12:35 -0700

    test not giving malmo commands on human

---
+++ tests/test_human.py
@@ -0,0 +1,44 @@
+import pytest
+import logging
+
+from mbag.evaluation.evaluator import MbagEvaluator
+from mbag.agents.hardcoded_agents import (
+    HardcodedResourceAgent,
+    HardcodedInventoryDonator,
+)
+from mbag.agents.heuristic_agents import NoopAgent
+from mbag.environment.goals.simple import BasicGoalGenerator
+
+logger = logging.getLogger(__name__)
+
+# @pytest.mark.uses_malmo
+def test_human_in_malmo():
+    """
+    Make sure the inventory agent can place blocks
+    """
+
+    evaluator = MbagEvaluator(
+        {
+            "world_size": (5, 6, 5),
+            "num_players": 1,
+            "horizon": 20,
+            "goal_generator": BasicGoalGenerator,
+            "goal_generator_config": {"pallette": True},
+            "malmo": {
+                "use_malmo": True,
+                "use_spectator": False,
+                "video_dir": None,
+            },
+            "players": [
+                {
+                    "is_human": True,
+                }
+            ],
+            "abilities": {"teleportation": False, "flying": True, "inf_blocks": False},
+        },
+        [
+            (HardcodedResourceAgent, {}),
+        ],
+    )
+    episode_info = evaluator.rollout()
+    assert episode_info.cumulative_reward == 0
