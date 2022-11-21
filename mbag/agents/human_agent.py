ca158890ff86b9ceef39e4a75b0193aec5bde3d7
Timothy G <timg51237@gmail.com>
2022-11-20 23:00:18 -0800

    initially create human agent

---
+++ mbag/agents/human_agent.py
@@ -0,0 +1,52 @@
+from abc import ABC
+from typing import Any, List, cast
+from mbag.environment.blocks import MinecraftBlocks
+import numpy as np
+
+from ..environment.types import MbagAction, MbagActionType, MbagObs, MbagActionTuple
+from ..environment.mbag_env import MbagConfigDict
+from .mbag_agent import MbagAgent
+
+
+class HumanAgent(MbagAgent):
+    """
+    An MBAG agent which chooses actions based on a queue that is fed in.
+    """
+
+    actions_queue: List[MbagAction]
+
+    def reset(self) -> None:
+        """
+        This method is called whenever a new episode starts; it can be used to clear
+        internal state or otherwise prepare for a new episode.
+        """
+        self.actions_queue = []
+
+    def get_action(self, obs: MbagObs) -> MbagActionTuple:
+        """
+        This should return an action ID to take in the environment. Either this or the
+        get_action_*_distribution methods should be overridden.
+        """
+        action_type, block_location, block_id = MbagAction.NOOP, 0, 0
+        if len(self.actions_queue > 0):
+            action_type, block_location, block_id = self.actions_queue.pop(0)
+        return action_type, block_location, block_id
+
+    def add_actions(self, incoming_actions: List[MbagAction]) -> None:
+        self.actions_queue.extend(incoming_actions)
+
+    def get_state(self) -> List[np.ndarray]:
+        """
+        Get the current state of this agent as a list of zero or more numpy arrays.
+        The agent should be able to be set back to its previous state by calling
+        set_state with the return value of this method.
+        """
+
+        return [self.actions_queue]
+
+    def set_state(self, state: List[np.ndarray]) -> None:
+        """
+        Restore the agent's state to what it was when get_state was called.
+        """
+
+        self.actions_queue = state[0]
