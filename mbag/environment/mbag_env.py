04a406bc9d2d82018e60d15b52e7a237ae2c621b
Cassidy Laidlaw <cassidy256@gmail.com>
2024-05-01 13:10:36 -0700

    Add warning if action inference is taking too long when playing with Malmo

--- mbag/environment/mbag_env.py
+++ mbag/environment/mbag_env.py
@@ -1036,6 +1036,12 @@ class MbagEnv(object):
         then update the environment state from the latest Malmo state.
         """
 
+        if time.time() - self.previous_step_timestamp > self.config["malmo"]["action_delay"]:
+            logger.warn(
+                "environment step took longer than Malmo action_delay; action_delay "
+                "may need to be increased to achieve consistent step rate"
+            )
+
         any_human_actions = False
         while True:
             human_actions, malmo_state = (
