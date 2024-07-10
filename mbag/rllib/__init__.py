6afcaf733f4d595bc36e58f3f057725901ba2607
Cassidy Laidlaw <cassidy256@gmail.com>
2024-07-10 09:57:13 -0700

    Fix issue with mixture model not getting registered

--- mbag/rllib/__init__.py
+++ mbag/rllib/__init__.py
@@ -1,6 +1,7 @@
 # Make sure everything is registered in RLLib catalogs.
 from . import alpha_zero  # noqa: F401
 from . import bc  # noqa: F401
+from . import mixture_model  # noqa: F401
 from . import policies  # noqa: F401
 from . import rllib_env  # noqa: F401
 from . import sacred_utils  # noqa: F401
