bfa587681f231d0c8e70bb9bb3135a2d782533e8
Cassidy Laidlaw <cassidy256@gmail.com>
2024-12-12 11:11:44 -0800

    Fix sacred error in evaluate script

--- mbag/scripts/evaluate.py
+++ mbag/scripts/evaluate.py
@@ -102,7 +102,7 @@ def human_alone():
     checkpoints = [assistant_checkpoint]  # noqa: F841
     policy_ids = [None]  # noqa: F841
     num_episodes = 1  # noqa: F841
-    algorithm_config_updates: List[dict] = [{}]  # noqa: F841
+    algorithm_config_updates = [{}]  # type: ignore # noqa: F841
     env_config_updates = {  # noqa: F841
         "goal_generator_config": {
             "goal_generator_config": {"subset": goal_set, "house_id": house_id}
